d3689536063704c433205ac9e1bb88f29045f0dc
Jeyasri17 <jeyasrivenkatarengan@gmail.com>
2022-12-29 23:21:24 +0530

    First Commit

---
+++ src/main/java/com/example/demo/CustomerLogApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CustomerLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerLogApplication.class, args);
+	}
+	public void run(String... args) throws Exception
+	{
+		
+	}
+
+}
